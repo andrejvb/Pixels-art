855f2e22171f4c947d0c3d1b72928a7136da6fc9
andrejvb <andre.jvb@gmail.com>
2022-10-04 20:54:52 -0300

    cumpri requisito 4

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 // variaveis globais
 const principal = document.getElementById('color-palette');
-
-
-
-
-
-
-
-
-
-
-
+const palleta = document.getElementsByClassName('color');
 
 
 // funções
+function randomCollors(){
+    const a = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const c = Math.floor(Math.random() * 256);
+    return `rgb(${a}, ${b}, ${c})`;
+}
+
+
 function createOptionsColor(){
     principal.style.display = 'flex';
 
@@ -24,30 +22,34 @@ function createOptionsColor(){
         createDiv.style.height = '50px';
         createDiv.style.border = 'solid 1px black';
         createDiv.className = 'color';
+        createDiv.style.backgroundColor = randomCollors();
         principal.appendChild(createDiv);
         
-    }
+    }    
     
-  
-    return principal
 }
 
 
 
 
-function colorPallet(){
-    const palleta = document.getElementsByClassName('color');
-    palleta[0].style.backgroundColor = 'black';
-    palleta[1].style.backgroundColor = 'red';
-    palleta[2].style.backgroundColor = 'green';
-    palleta[3].style.backgroundColor = 'RebeccaPurple';
-    
-    
-    
 
+
+
+function createButton(){
+    const criaBotao = document.createElement('button')
+    criaBotao.id = 'button-random-color';
+    criaBotao.style.marginTop = '50px';
+    criaBotao.innerHTML = 'Cores aleatórias';
+    principal.appendChild(criaBotao);
+    palleta[0].style.backgroundColor = 'black';  
+    criaBotao.addEventListener('click', function colorPallet(){
+          
+        for (let index = 1; index < 4; index += 1) {
+            palleta[index].style.backgroundColor = randomCollors();           
+        }
+    
+    })
 }
-
-
 
 
 
@@ -62,4 +64,4 @@ function colorPallet(){
 
 // executa funções
 createOptionsColor();
-colorPallet();
+createButton();
